adf95a80d9f92ecd5e800b11a213d8d1d780c4d3
suyeonlee333333 <suyeon2005816@naver.com>
2025-07-17 20:42:53 +0900

    ajax1까지

--- js/tab.js
+++ js/tab.js
@@ -1,30 +1,53 @@
 
-$('.tab-button').eq(0).on('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(0).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(0).addClass('show');
+// $('.tab-button').eq(0).on('click', function() {
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(0).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(0).addClass('show');
+// });
+
+// $('.tab-button').eq(1).on('click', function() {
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(1).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(1).addClass('show');
+// });
+
+// $('.tab-button').eq(2).on('click', function() {
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(2).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(2).addClass('show');
+// });
+
+// for (let i = 0; i < $('.tab-button').length; i++) {
+//     $('.tab-button').eq(i).on('click', function() {
+//         $('.tab-button').removeClass('orange');
+//         $('.tab-button').eq(i).addClass('orange');
+//         $('.tab-content').removeClass('show');
+//         $('.tab-content').eq(i).addClass('show');
+// });
+// };
+
+$('.list').click(function(e) {
+    if (e.target == document.querySelectorAll('.tab-button')[0]) {
+        탭열기(0);
+    }
+    if (e.target == document.querySelectorAll('.tab-button')[1]) {
+        탭열기(1);
+    }
+    if (e.target == document.querySelectorAll('.tab-button')[2]) {
+        탭열기(2);
+    }
 });
 
-$('.tab-button').eq(1).on('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(1).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(1).addClass('show');
-});
+$('.list').click(function(e) {
+    탭열기(e.target.dataset.id)
+})
 
-$('.tab-button').eq(2).on('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(2).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(2).addClass('show');
-});
-
-for (let i = 0; i < $('.tab-button').length; i++) {
-    $('.tab-button').eq(i).on('click', function() {
+function 탭열기 (i) {
     $('.tab-button').removeClass('orange');
     $('.tab-button').eq(i).addClass('orange');
     $('.tab-content').removeClass('show');
     $('.tab-content').eq(i).addClass('show');
-});
 };
